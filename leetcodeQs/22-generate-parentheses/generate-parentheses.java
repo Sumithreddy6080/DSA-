3bf532e935637108b2f55122bd524654e6389ce8
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-03-19 22:28:00 +0530

    Time: 1 ms (82.84%) | Memory: 43.3 MB (59.92%) - LeetSync

--- leetcodeQs/22-generate-parentheses/generate-parentheses.java
+++ leetcodeQs/22-generate-parentheses/generate-parentheses.java
@@ -8,16 +8,15 @@ class Solution {
         return res;
     }
     void solve(int open,int close,String temp, List<String> res){
-        if( close ==0 && open==0 && open+close==0){
+        if(open==0 && close==0){
             res.add(temp);
             return;
         }
-        if(open>0){
+        if(open !=0){
             solve(open-1,close,temp+"(",res);
         }
         if(open<close){
             solve(open,close-1,temp+")",res);
         }
-        return;
     }
 }
