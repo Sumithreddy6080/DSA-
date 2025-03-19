dc7bef1ad050bf51d7e545b18884ac7265e9e481
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-03-19 22:45:08 +0530

    Time: 1 ms (40.38%) | Memory: 40.8 MB (63.52%) - LeetSync

--- leetcodeQs/231-power-of-two/power-of-two.java
+++ leetcodeQs/231-power-of-two/power-of-two.java
@@ -1,7 +1,16 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
- 
-        return  ((n > 0) && ((n & (n - 1)) == 0));
+        boolean flag = true && n!=0;
+        while(n!=0){
+            if(n%2==0){
+                n= n/2;
+            }else{
+                if(n==1) break;
+                flag=false;
+                break;
+            }
+        }
+        return flag;
         
     }
 }
