2ac32256f5a02093a39de3d9d5ccd4ed966aa993
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-03-22 00:53:28 +0530

    Time: 2 ms (99.11%) | Memory: 43 MB (76.72%) - LeetSync

--- leetcodeQs/125-valid-palindrome/valid-palindrome.java
+++ leetcodeQs/125-valid-palindrome/valid-palindrome.java
@@ -1,15 +1,20 @@
 class Solution {
-public boolean isPalindrome(String s) {
-StringBuilder sb = new StringBuilder();
-
-    for(char ch : s.toCharArray()){
-        if(Character.isLetterOrDigit(ch)){
-            sb.append(Character.toLowerCase(ch));
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++; 
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+            
+            i++;
+            j--;
         }
+        
+        return true;
     }
-    if(sb.toString().equals(sb.reverse().toString())){
-    return true;
-    }
-    return false;
-}
 }
