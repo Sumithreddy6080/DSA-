8894778419926f99fd6d2e33fa84611cf55bec84
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-03-21 15:02:24 +0530

    Time: 2 ms (83.15%) | Memory: 44.5 MB (81.05%) - LeetSync

--- leetcodeQs/39-combination-sum/combination-sum.java
+++ leetcodeQs/39-combination-sum/combination-sum.java
@@ -2,23 +2,24 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> ot = new ArrayList<>();
-        solve(candidates,target,0,0,ot,ans);
+        solve(candidates,target,0,0,ans,ot);
         return ans;
     }
-    void solve(int[] candidates, int target,int index,int sum,List<Integer> ot,List<List<Integer>> ans){
-        if(index>=candidates.length){
+    void solve(int[] candidates, int target,int sum,int index, List<List<Integer>> ans,  List<Integer> ds){
+        if(index==candidates.length){
             if(sum==target){
-                ans.add(new ArrayList<>(ot));
+                ans.add(new ArrayList<>(ds));
             }
             return;
         }
-        if(sum>target) return;
-        if(candidates[index]<=target){
-            ot.add(candidates[index]);
-            solve(candidates,target,index,sum+candidates[index],ot,ans);
-            ot.remove(ot.size()-1);
-        }
-          solve(candidates,target,index+1,sum,ot,ans);
 
+        if(candidates[index]+sum<=target){
+            ds.add(candidates[index]);
+            solve(candidates,target,candidates[index]+sum,index,ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        solve(candidates,target,sum,index+1,ans,ds);
+    
+   
     }
 }
