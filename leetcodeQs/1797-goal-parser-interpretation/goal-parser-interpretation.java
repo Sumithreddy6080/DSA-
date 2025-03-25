03e7397d87252d9c12e77fc49459810f3358e769
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-03-26 02:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (88.46%) - LeetSync

--- leetcodeQs/1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ leetcodeQs/1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -1,7 +1,24 @@
 class Solution {
     public String interpret(String command) {
-        command=command.replace("()","o");
-        command=command.replace("(al)","al");
-        return command;
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+
+        while (i < command.length()) {
+            char ch = command.charAt(i);
+            if (ch == 'G') {
+                sb.append("G");
+                i++;
+            } else if (ch == '(') {
+                if (command.charAt(i + 1) == ')') {
+                    sb.append("o");
+                    i = i + 2;
+                } else {
+                    sb.append("al");
+                    i = i + 4;
+                }
+            }
+        }
+
+        return sb.toString();
     }
 }
