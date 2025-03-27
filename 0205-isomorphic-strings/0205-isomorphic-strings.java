6105cb93183caeaac1693a8eabadc7347ec14b22
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-03-28 00:33:59 +0530

    0205-ISOMORPHIC-STRINGS Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,26 +1,12 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int[] indexS = new int[200]; 
-        int[] indexT = new int[200]; 
-        
-        int len = s.length();
-        
-      
-        if(len != t.length()) {
-            return false;
+        int[] hash1 = new int[26];
+        int[] hash2 = new int[26];
+        for(int i =0; i<s.length();i++){
+            if(hash2[(int)(t.charAt(i)-'a')] != hash1[(int)(s.charAt(i)-'a')]) return false;
+            hash1[(int)(s.charAt(i)-'a')]++;
+            hash2[(int)(t.charAt(i)-'a')]++;
         }
-        
-        for(int i = 0; i < len; i++) {
-       
-            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]) {
-                return false; 
-            }
-            
-            indexS[s.charAt(i)] = i + 1; 
-            indexT[t.charAt(i)] = i + 1; 
-        }
-        
         return true;
     }
 }
-
