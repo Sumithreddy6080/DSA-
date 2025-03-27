33b6e210d678d451c63135ebec00739ae57b0a4d
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-03-28 00:15:12 +0530

    0812-ROTATE-STRING Stats: Time: 1 ms (19.85%), Space: 41.8 MB (27.07%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -2,12 +2,8 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         for(int i =0;i<goal.length();i++){
             if(s.equals(goal)) return true;
-            s= rotate(s);
+            s= s.substring(1,s.length())+s.substring(0,1);
         }
         return false;
-    }
-    String rotate(String s){
-        String ans = s.substring(1,s.length())+s.substring(0,1);
-        return ans;
     }
 }
