3ad79574aec4c1ec617b8f7b10f27ecd99ce2cd3
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-03-28 20:28:15 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 23 ms (46.26%), Space: 45.3 MB (42.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,28 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
-        // if(s.length()==1) return s;
-        StringBuilder ans = new  StringBuilder("");
-        int maxlen =0;
-        for(int i =0; i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                String sb = s.substring(i,j+1);
-                if(isPalin(sb) && sb.length()>maxlen){
-                    maxlen = sb.length();
-                    ans = new StringBuilder(sb);
+        String ans = "";
+        for(int i=0;i<s.length();i++){
+            int l=i;
+            int h=i;
+            while((l>=0 && h<s.length()) && (s.charAt(l) == s.charAt(h))){
+                if((h-l)>=ans.length()){
+                    ans=s.substring(l,h+1);
                 }
+                l--;
+                h++;
             }
+             l=i;
+             h=i+1;
+            while((l>=0 && h<s.length()) && (s.charAt(l) == s.charAt(h))){
+                if((h-l)>=ans.length()){
+                    ans=s.substring(l,h+1);
+                }
+                l--;
+                h++;
+            }
+
         }
-        return ans.toString();
-    }
-    boolean isPalin(String str){
-        int l=0;
-        int h=str.length()-1;
-        if(h<=0) return true;
-        while(l<h){
-            if(str.charAt(l) != str.charAt(h)) return false;
-            l++;
-            h--;
-        }
-        return true;
+        return ans;
     }
 }
