df2dab028123e0ce99a9e327a7a5df0bb62c3d76
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-04-06 02:47:02 +0530

    0328-ODD-EVEN-LINKED-LIST Stats: Time: 0 ms (100%), Space: 44.7 MB (48.77%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head ==null || head.next == null) return head;
+        ListNode h1 = head;
+        ListNode h2 = head.next;
+        ListNode even = h2;
+        while(h1!=null && h2 !=null){
+            h1.next = h2.next;
+            h1= h1.next;
+            if(h1 !=null){
+            h2.next = h1.next;
+            h2 = h2.next;
+            }
+        }
+        ListNode tail =head;
+        while(tail.next != null) tail = tail.next;
+        tail.next = even;
+        return head;
+    }
+}
